c6f2ff56bd6d0cc4311e315e54fdced6d15bd0f7
Chase Van Halen <cvanhalen@perscholas.org>
2023-06-06 17:01:13 -0500

    add random num functionality to plant server, obviously

--- website_project/public/index.js
+++ website_project/public/index.js
@@ -3,6 +3,18 @@
 
 let ulElement = document.querySelector('.ul-container');
 
+let randomButton = document.getElementById('random-button');
+
+randomButton.addEventListener('click', () => {
+    fetch('http://localhost:3000/random').then((res) => {
+        res.json().then((finalData) => {
+            let myLi = document.createElement('li');
+            myLi.textContent = finalData.data;
+            ulElement.appendChild(myLi)
+        })
+    })
+});
+
 fetch('http://localhost:3000/plants').then((res) => {
     res.json().then((data) => {
 
@@ -14,15 +26,15 @@ fetch('http://localhost:3000/plants').then((res) => {
     })
 })
 
-setInterval(() => {
-    fetch(`http://localhost:3000/plants/2`).then((res) => {
-        console.log(res)
-        res.json().then((data) => {
-                let myLi = document.createElement('li');
-                myLi.textContent = data.string;
-                ulElement.appendChild(myLi)
-        })
-    })
-}, 2000)
+// setInterval(() => {
+//     fetch(`http://localhost:3000/plants/2`).then((res) => {
+//         console.log(res)
+//         res.json().then((data) => {
+//                 let myLi = document.createElement('li');
+//                 myLi.textContent = data.string;
+//                 ulElement.appendChild(myLi)
+//         })
+//     })
+// }, 2000)
 
 ulElement.innerHTML = "<li>I am a LI tag!</li>"
--- website_project/website_server.js
+++ website_project/website_server.js
@@ -14,6 +14,12 @@ app.get("/plants/:indexOfPlant", (req, res) => {
     res.send({string: plants[req.params.indexOfPlant]})
 })
 
+app.get('/random', (req, res) => {
+    let randomNum = Math.floor(Math.random()*2000)
+    res.send({data: randomNum});
+});
+
+
 app.listen(3000, () => {
     console.log("sever listening on port ", 3000)
 });
